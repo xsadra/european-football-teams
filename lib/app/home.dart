dde5841e4a3652050aec019fa6077074a8e84284
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-04 00:09:00 +0100

    create Home page

---
+++ lib/app/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("all about clubs"),
+        ),
+        body: Container(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
