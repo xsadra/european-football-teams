6d4710b196133f9970dc248417c4ef9fd48f065c
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-04 00:09:17 +0100

    Change the Main Color to '#01C13B'

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'European Football Teams',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: Container(
-        color: Colors.white,
-      ),
+      theme: ThemeData().copyWith(primaryColor: kMainColor),
+      home:Home(),
     );
   }
 }
