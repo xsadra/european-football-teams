f0fd0ade8a90d62016026d258bf5d1635ee97060
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-10 14:19:38 +0100

    Create a method to got Clubs from API

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:football/data/club.dart';
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -7,6 +10,23 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   bool ascSort = true;
+
+  Future<List<Club>> _getClubs() async {
+
+    String path = "https://public.allaboutapps.at/hiring/clubs.json";
+    final res = await http.get(path);
+    var body = res.body;
+    var decode = json.decode(body) as List;
+    var asMap = decode.asMap();
+    List<Club> list = asMap.values.map((e) => Club.fromJson(e)).toList();
+    if (ascSort) {
+      list.sort((a, b) => a.name.compareTo(b.name));
+    } else {
+      list.sort((a, b) => b.name.compareTo(a.name));
+    }
+    return list;
+  }
+
 
   @override
   Widget build(BuildContext context) {
