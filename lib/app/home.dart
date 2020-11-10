6835a69cc7f9fb6998f9b59881e29cefcc1ba917
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-10 13:52:52 +0100

    Add sort action button to AppBar

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -6,17 +6,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool ascSort = true;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           title: Text("all about clubs"),
+          actions: [
+            IconButton(
+              //TODO : change the icon
+              icon: Icon((ascSort) ? Icons.arrow_upward : Icons.arrow_downward),
+              tooltip: 'Sort',
+              onPressed: () {
+                toggleSort();
+              },
+            )
+          ],
         ),
         body: Container(
           color: Colors.white,
         ),
       ),
     );
+  }
+
+  void toggleSort() {
+    setState(() {
+      ascSort = !ascSort;
+    });
   }
 }
