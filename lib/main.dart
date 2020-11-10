910c38769519bd3a8e37aab3194832cf0d8ff6e7
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-10 18:22:49 +0100

    Add routes to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:football/app/home.dart';
+import 'package:football/app/view.dart';
 import 'package:football/constant/colors.dart';
 
 void main() {
@@ -8,12 +9,18 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
+  static String id = 'home';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'European Football Teams',
       theme: ThemeData().copyWith(primaryColor: kMainColor),
       home: Home(),
+      routes: {
+        MainApp.id: (context) => MainApp(),
+        ViewClubScreen.id: (context) => ViewClubScreen()
+      },
       supportedLocales: [
         Locale('en', 'GB'),
         Locale('de', 'AT'),
