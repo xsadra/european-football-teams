26a42bc8f4a3b0d979deb998629f9aa61828a7aa
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-12 21:57:57 +0100

    Catch and throw error while GET data from API

--- lib/service/club_repository.dart
+++ lib/service/club_repository.dart
@@ -3,14 +3,20 @@ import 'package:football/model/club.dart';
 import 'package:football/service/json_loader.dart';
 
 class ClubRepository {
-  static Future<List<Club>> getClubs(bool ascSort) async {
+  static Future<List<Club>> getClubs(bool ascOrder) async {
     String path = await ConfigsLoader.getApiUrl();
     var dio = Dio();
-    Response response = await dio.get(path);
+    Response response = await dio
+        .get(path)
+        .timeout(Duration(seconds: 30))
+        .catchError((onError) {
+      throw onError;
+    });
+    
     List<Club> list =
         (response.data as List).map((item) => Club.fromJson(item)).toList();
 
-    if (ascSort) {
+    if (ascOrder) {
       list.sort((a, b) => a.name.compareTo(b.name));
     } else {
       list.sort((a, b) => b.name.compareTo(a.name));
