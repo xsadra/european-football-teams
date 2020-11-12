47bb74cd53fe34bd7b8bc6e47b6505971b856f38
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-12 22:09:45 +0100

    Add some Text style to constants

---
+++ lib/constant/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xFF01C13B);
+
+TextStyle kNotifyPanelTextStyle() => TextStyle(
+      fontSize: 21.0,
+      letterSpacing:2,
+    );
