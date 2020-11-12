4b8753056e97817f5065c15065d4133423c7fc64
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-12 02:07:09 +0100

    Use 'ConfigsLoader' to load API URL in 'ClubRepository'

--- lib/service/club_repository.dart
+++ lib/service/club_repository.dart
@@ -1,22 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:football/model/club.dart';
+import 'package:football/service/json_loader.dart';
 
-class ClubRepository{
-
- static Future<List<Club>> getClubs(bool ascSort) async {
-    String path = "https://public.allaboutapps.at/hiring/clubs.json";
+class ClubRepository {
+  static Future<List<Club>> getClubs(bool ascSort) async {
+    String path = await ConfigsLoader.getApiUrl();
     var dio = Dio();
     Response response = await dio.get(path);
     List<Club> list =
-    (response.data as List).map((item) => Club.fromJson(item)).toList();
+        (response.data as List).map((item) => Club.fromJson(item)).toList();
 
     if (ascSort) {
       list.sort((a, b) => a.name.compareTo(b.name));
     } else {
       list.sort((a, b) => b.name.compareTo(a.name));
     }
-    print('length:');
-    print(list.length);
+
     return list;
   }
 }
