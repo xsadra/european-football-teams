183b291fc24c5cfbe2e4104b14e1ff6c767285a1
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-12 23:02:28 +0100

    Clean the code

--- lib/app/view.dart
+++ lib/app/view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:football/constant/constants.dart';
 import 'package:football/locale/app_localization.dart';
 import 'package:football/model/club.dart';
+import 'package:football/widget/detail_text.dart';
 
 class ViewClubScreen extends StatelessWidget {
   static String id = 'view_screen';
@@ -13,6 +14,11 @@ class ViewClubScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     AppLocalization msg = AppLocalization.of(context);
 
+    String firstMessage = msg.firstMessage
+        .replaceFirst('xCountry', club.country)
+        .replaceFirst('xValue', club.value.toString());
+    String secondMessage = msg.secondMessage
+        .replaceFirst('xVictorious', club.europeanTitles.toString());
     return MaterialApp(
       theme: ThemeData().copyWith(primaryColor: kMainColor),
       home: Scaffold(
@@ -40,74 +46,61 @@ class ViewClubScreen extends StatelessWidget {
                         ? Image.network(club.image)
                         : Icon(Icons.error, color: Colors.white70, size: 120.0),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(left: 16.0, bottom: 16.0),
-                        child: Text(
-                          club.country,
-                          style: TextStyle(
-                            color: Colors.white70,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
-                  )
+                  clubCountryText(club.country)
                 ],
               ),
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RichText(
-                    text: TextSpan(
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(text: msg.club + ' '),
-                        TextSpan(
-                            text: club.name,
-                            style: TextStyle(fontWeight: FontWeight.bold)),
-                        TextSpan(
-                          text: msg.firstMessage
-                              .replaceFirst('xCountry', club.country)
-                              .replaceFirst('xValue', club.value.toString()),
-                        )
-                      ],
-                    ),
-                  ),
+                DetailText(
+                  children: <TextSpan>[
+                    TextSpan(text: msg.club + ' '),
+                    clubNameText(),
+                    TextSpan(
+                      text: firstMessage,
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RichText(
-                    text: TextSpan(
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: club.name,
-                            style: TextStyle(fontWeight: FontWeight.bold)),
-                        TextSpan(
-                          text: msg.secondMessage.replaceFirst(
-                              'xVictorious', club.europeanTitles.toString()),
-                        )
-                      ],
-                    ),
-                  ),
+                DetailText(
+                  children: <TextSpan>[
+                    clubNameText(),
+                    TextSpan(
+                      text: secondMessage,
+                    )
+                  ],
                 ),
               ],
             )
           ],
         ),
       ),
+    );
+  }
+
+  Row clubCountryText(String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, bottom: 16.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white70,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  TextSpan clubNameText() {
+    return TextSpan(
+      text: club.name,
+      style: TextStyle(fontWeight: FontWeight.bold),
     );
   }
 }
