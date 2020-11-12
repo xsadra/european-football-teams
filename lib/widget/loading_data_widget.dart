ad5dbf2cc3bb17a720fdd1e15baec86ec50cacb2
Sadra <31988724+xsadra@users.noreply.github.com>
2020-11-12 22:10:58 +0100

    Refactor 'NotifyPanel' to show different text

--- lib/widget/loading_data_widget.dart
+++ lib/widget/loading_data_widget.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:football/constant/constants.dart';
 
-class LoadingDataWidget extends StatelessWidget {
-  const LoadingDataWidget({
-    Key key,
-  }) : super(key: key);
+class NotifyPanel extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  const NotifyPanel({Key key, @required this.text, this.color = Colors.black45})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
         child: Text(
-          "Lade Daten ...",
-          style: TextStyle(
-              color: Colors.black45, fontSize: 21.0, letterSpacing: 3),
+          text,
+          style: kNotifyPanelTextStyle().copyWith(color: color),
         ),
       ),
     );
